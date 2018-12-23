1c74b6001d8d5fd06484ab66f2732990d74eadf1
james <james@librem.slim>
2018-12-23 12:02:26 -0600

    Added Unlock and Qr view for EOS Bip39 keypair.

---
+++ src/coin-exports.js
@@ -0,0 +1,9 @@
+module.exports = {
+  HDKey: require('hdkey'),
+  coinstring: require('coinstring'),
+  safeBuffer: require('safe-buffer'),
+  secp256k1: require('secp256k1'),
+  createHash: require('create-hash'),
+  bip39Checker: require('bip39-checker'),
+  base58: require('bs58')
+}
